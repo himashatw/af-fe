61028119259d05b7bf9a20a35f460e724b0012da
ushan1997 <ushananuka5@gmail.com>
2021-06-28 00:57:48 +0530

    added viewResearch.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Header from './Header/Header'
+import viewResearches from './Reviewer/viewResearches'
 
 const App = () => {
     return (
         <Router>
             <div>
-                <Header />
+                <Header/> 
                 <Switch>
                     <Route exact path="/">
                         <h1>This is App.js File</h1>
                     </Route>
+                    <Route path="/viewResearches" component ={viewResearches}/>
                 </Switch>
             </div>
         </Router>
