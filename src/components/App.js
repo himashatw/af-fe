002ed0455b9f7689762a2eb0bb99d241524668a5
Himasha Weerasinghe <himashatw@gmail.com>
2021-06-27 14:56:50 +0530

    nav bar added

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Header from './Header/Header'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./Header/Header";
+import "./App.css";
 
 const App = () => {
-    return (
-        <Router>
-            <div>
-                <Header />
-                <Switch>
-                    <Route exact path="/">
-                        <h1>This is App.js File</h1>
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <h1>This is App.js File</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,29 +1,84 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect } from "react";
+import HomeButton from "../HomeButton/HomeButton";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <div>
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
-            <ul id="dropdown1" class="dropdown-content">
-                <li><a href="#!">one</a></li>
-                <li><a href="#!">two</a></li>
-                <li class="divider"></li>
-                <li><a href="#!">three</a></li>
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
+
+  return (
+    <div>
+      <>
+        <nav className="navbar">
+          <div className="navbar-container">
+            <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+              CMS
+              <i className="fab fa-typo3" />
+            </Link>
+            <div className="menu-icon" onClick={handleClick}>
+              <i className={click ? "fas fa-times" : "fas fa-bars"} />
+            </div>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              <li className="nav-item">
+                <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/news"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  News
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/articles"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  Speakers
+                </Link>
+              </li>
+
+              <li>
+                <Link
+                  to="/signup"
+                  className="nav-links-mobile"
+                  onClick={closeMobileMenu}
+                >
+                  Sign Up
+                </Link>
+              </li>
             </ul>
-            <nav>
-                <div class="nav-wrapper">
-                    <a href="#!" class="brand-logo">Logo</a>
-                    <ul class="right hide-on-med-and-down">
-                        <li><a href="sass.html">Sass</a></li>
-                        <li><a href="badges.html">Components</a></li>
-                        <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li>
-                    </ul>
-                </div>
-            </nav>
-
-        </div>
-    );
+            {button && (
+              <HomeButton buttonStyle="btn--outline">SIGN UP</HomeButton>
+            )}
+          </div>
+        </nav>
+      </>
+    </div>
+  );
 };
 
 export default Header;
