4baf14d84b97070b642fc2072ede49188314f863
Punsisi Perera <punsisi.subhani@gmail.com>
2021-06-28 01:00:28 +0530

    Commit1

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Header/Header";
 import "./App.css";
+import EditorDashboard from "./EditorWorkspace/EditorDashboard";
 
 const App = () => {
   return (
@@ -11,6 +12,9 @@ const App = () => {
         <Switch>
           <Route exact path="/">
             <h1>This is App.js File</h1>
+          </Route>
+          <Route exact path="/editor">
+            <EditorDashboard />
           </Route>
         </Switch>
       </div>
