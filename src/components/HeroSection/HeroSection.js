20c8e5b037295edbabeb535e126ee74db3ba4903
Himasha Weerasinghe <himashatw@gmail.com>
2021-06-28 20:53:16 +0530

    react-icons&admin home added, header styling fixed

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -5,8 +5,7 @@ import HomeButton from "../HomeButton/HomeButton";
 const HeroSection = () => {
   return (
     <div className="hero-container">
-          {/* <video src="./video-2.mp4" autoPlay loop muted /> */}
-      <h1>ICAC 2.0</h1>
+      <h1>ICAF 1.0</h1>
       <p>Starting from 30th June</p>
       <div className="hero-btns">
         <HomeButton
@@ -14,16 +13,8 @@ const HeroSection = () => {
           buttonStyle="btn--outline"
           buttonSize="btn--large"
         >
-          More Details
+          Register Now!
         </HomeButton>
-        {/* <HomeButton
-          className="btns"
-          buttonStyle="btn--primary"
-          buttonSize="btn--large"
-          onClick={console.log("hey")}
-        >
-          WATCH TRAILER <i className="far fa-play-circle" />
-        </HomeButton> */}
       </div>
     </div>
   );
