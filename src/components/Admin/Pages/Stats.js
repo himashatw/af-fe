4817e8ba0c09a1534b6350b15f73ac699f8a6b2f
Himasha Weerasinghe <himashatw@gmail.com>
2021-06-30 00:45:56 +0530

    stats data added

--- src/components/Admin/Pages/Stats.js
+++ src/components/Admin/Pages/Stats.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "../../../services/axios";
 
 const Stats = () => {
+  const [statObj, setStatObj] = useState({});
+  useEffect(async () => {
+    await axios.get("/countall").then((respone) => {
+      setStatObj(respone.data.countObj);
+    });
+  }, []);
+  console.log(statObj);
   return (
     <div className="stats">
       <div className="container mt-2" style={{ border: "1px solid black" }}>
@@ -8,52 +16,69 @@ const Stats = () => {
           <h1 style={{ fontSize: "1.5rem" }}>Stats</h1>
         </center>
       </div>
-      <div className="container mt-2" style={{ border: "1px solid black" }}>
+      <div className="container mt-5" style={{ border: "1px solid black" }}>
         <div className="row row-cols-1 row-cols-md-2 g-4">
           <div className="col">
             <div className="card">
               <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
+                <div
+                  className="container"
+                  style={{ display: "inline-block", textAlign: "center" }}
+                >
+                  <h5 className="card-title">Attendees</h5>
+                  <i className="fas fa-users"></i>
+                </div>
+                <center>
+                  <h1 className="card-text mt-3">{statObj.attendees}</h1>
+                </center>
               </div>
             </div>
           </div>
           <div className="col">
             <div className="card">
               <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
+                <div
+                  className="container"
+                  style={{ display: "inline-block", textAlign: "center" }}
+                >
+                  <h5 className="card-title">Researchers</h5>
+                  <i class="fas fa-users"></i>
+                </div>
+                <center>
+                  <h1 className="card-text mt-3">{statObj.researchers}</h1>
+                </center>
               </div>
             </div>
           </div>
           <div className="col">
             <div className="card">
               <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content.
-                </p>
+                <div
+                  className="container"
+                  style={{ display: "inline-block", textAlign: "center" }}
+                >
+                  <h5 className="card-title">Workshops</h5>
+                  <i class="fas fa-users"></i>
+                </div>
+                <center>
+                  <h1 className="card-text mt-3">{statObj.workshops}</h1>
+                </center>
               </div>
             </div>
           </div>
           <div className="col">
             <div className="card">
               <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
+                <div
+                  className="container"
+                  style={{ display: "inline-block", textAlign: "center" }}
+                >
+                  <h5 className="card-title">Research Papers</h5>
+                  <i class="fas fa-users"></i>
+                </div>
+                <center>
+                  <h1 className="card-text mt-3">{statObj.papers}</h1>
+                </center>
               </div>
             </div>
           </div>
