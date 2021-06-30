a13e0a1cf6ae273a27e61414d411f6a925837673
Himasha Weerasinghe <himashatw@gmail.com>
2021-06-30 13:35:19 +0530

    alignment fixed

---
+++ src/components/Admin/Pages/Speakers.js
@@ -0,0 +1,23 @@
+import React from "react";
+import SlideShow from "../SlideShow/SlideShow";
+import axios from "../../../services/axios";
+
+const Speakers = () => {
+  return (
+    <div className="speakers">
+      <div className="container mt-2" style={{ border: "1px solid black" }}>
+        <center>
+          <h1 style={{ fontSize: "1.5rem" }}>Keynote Speakers</h1>
+        </center>
+      </div>
+      <div className="container mt-2" style={{ border: "1px solid black" }}>
+        <></>
+      </div>
+      <div className="container mt-2" style={{ border: "1px solid black" }}>
+        <></>
+      </div>
+    </div>
+  );
+};
+
+export default Speakers;
