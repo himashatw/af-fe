13919bc9900960a195bc940a020881f08f92c543
ushan1997 <ushananuka5@gmail.com>
2021-06-30 20:46:12 +0530

    fixed css

--- src/components/DownloadPage/DashboardForDownload.js
+++ src/components/DownloadPage/DashboardForDownload.js
@@ -7,7 +7,7 @@ const downloadDashboard = () => {
     return (
         <div className="dash-usha-container">
             <br />
-            <center><h2>Welcome to ICAF 1.0 Download Page</h2></center>
+            <div className="ushan-para"><center><h2>Welcome to ICAF 1.0 Download Page</h2></center></div>
             <div>
                 <table className="table">
                     <tbody>
