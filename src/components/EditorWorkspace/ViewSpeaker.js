4c33fdc08f43be4cde839799780a4d860216d3fb
Punsisi Perera <punsisi.subhani@gmail.com>
2021-06-30 14:58:56 +0530

    finalized component of ViewSpeaker

--- src/components/EditorWorkspace/ViewSpeaker.js
+++ src/components/EditorWorkspace/ViewSpeaker.js
@@ -1,66 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "../../services/axios";
 
 const ViewSpeaker = () =>{
+
+const [speaker,setSpeaker] = useState([])
+useEffect(()=>{
+  axios.get("/editor/viewSpeaker").then((res) => {
+    console.log(res.data.speaker);
+    setSpeaker(res.data.speaker)
+  })
+},[])
+
     return(
+
 <div className="card-header">
-<h4>View Keynote Speakers</h4>
+  
+<h4>View Added Keynote Speakers </h4>
 <div className="container">
-        <div className="position-absolute top-50 start-50" style={{  marginLeft: "20%", marginTop:"10%" }}>
+<div className="position-absolute top-50 start-50" style={{  marginLeft: "10%", marginTop:"5%" }}>
 <div  className="shadow p-3 mb-5 bg-body rounded" >
 <table class="table caption-top">
   <thead>
     <tr>
-      <th scope="col">#</th>
       <th scope="col">Name</th>
       <th scope="col">Description</th>
-      <th scope="col">Speaker's Photo</th>
+      <th scope="col">Headshot</th>
       <th scope="col">Action</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>
-      <Link to="/editor/editSpeaker" className="btn btn-outline-success">
-              Edit 
-      </Link>
-      <Link to="/editor/deleteSpeaker" className="btn btn-outline-danger">
-              Delete
-      </Link>
-      </td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>
-      <Link to="/editor/editSpeaker" className="btn btn-outline-success">
-              Edit 
-      </Link>
-      <Link to="/editor/deleteSpeaker" className="btn btn-outline-danger">
-              Delete
-      </Link>
-      </td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-      <td>
-      <Link to="/editor/editSpeaker" className="btn btn-outline-success">
-              Edit 
-      </Link>
-      <Link to="/editor/deleteSpeaker" className="btn btn-outline-danger">
-              Delete
-      </Link>
-      </td>
-    </tr>
+  {
+    speaker.map(item=>{
+      return(
+        <tr>
+        <td>{item.name}</td>
+        <td>{item.description}</td>
+        <td><img src = {item.imgURL} width="200" height="200" /></td>
+        <td>
+        <Link to="/editor/editSpeaker" className="btn btn-outline-success">
+                Edit 
+        </Link>
+        <Link to="/editor/deleteSpeaker" className="btn btn-outline-danger">
+                Delete
+        </Link>
+        </td>
+      </tr>
+      )
+    })
+  }
   </tbody>
 </table>
 
